86391c15c2f7c98fe335748a20c2acbcc0523f07
Todd Persen <todd.persen@gmail.com>
2013-11-19 10:12:58 -0500

    Add blog index.

--- config.rb
+++ config.rb
@@ -28,6 +28,8 @@ with_layout :blog do
   page "/blog/*"
 end
 
+page "/blog/index.html", :layout => false
+
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
